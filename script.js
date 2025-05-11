f95c8d8472929ca3e407179aff700d6733926bc8
Alejandro Berguecio <134975938+alberguecio@users.noreply.github.com>
2025-05-11 18:04:31 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 // Navegación móvil
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.nav-links');
-const navLinks = document.querySelectorAll('.nav-links li');
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
 
-burger.addEventListener('click', () => {
     // Toggle Nav
-    nav.classList.toggle('nav-active');
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('active');
 
-    // Animate Links
-    navLinks.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = '';
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-        }
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
     });
 
-    // Burger Animation
-    burger.classList.toggle('toggle');
+    // Cerrar menú al hacer clic en un enlace
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            nav.classList.remove('active');
+            burger.classList.remove('toggle');
+            navLinks.forEach(link => {
+                link.style.animation = '';
+            });
+        });
+    });
 });
 
 // Smooth Scrolling
